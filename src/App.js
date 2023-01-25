6419b1304b6b809dab06d0c49e8676c5827437fd
hugo ROBERT <hugo@macbook-air.home>
2023-01-25 04:08:39 +0100

    top of the page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "./particles.json";
 function App() {
   const scrollToToProject = () => {
     window.scrollTo({
-      top: 800,
+      top: 700,
       behavior: "smooth",
     });
   };
@@ -26,18 +26,24 @@ function App() {
             id="particles-js"
             className="absolute h-screen w-screen z-10"
           ></div>
-          <div className="flex flex-col pt-60 pl-24 h-fit w-fit font-mono z-20">
-            <div className=" z-20">hi, my name is</div>
-            <div className=" z-20 text-6xl font-bold mt-5 ">hugo Robert.</div>
-            <div className=" z-20 flex flex-row text-7xl mt-20 font-bold">
-              <div className=" z-20">I'm a &nbsp;</div>
-              <div className=" z-20 text-yellow-500">Software Engineer</div>
+          <div className="flex flex-col md:flex-col pt-40 md:pt-60 pl-12 lg:pl-20 h-fit font-mono z-20">
+            <div className="text-xs md:text-base z-20">hi, my name is</div>
+            <div className=" z-20 text-3xl md:text-6xl font-bold md:mt-5 ">
+              hugo Robert.
             </div>
-            <div className=" z-20 mt-20 w-1/2">
+            <div className=" z-20 flex flex-col md:flex-row mt-8 md:mt-20 font-bold">
+              <div className="text-lg md:text-5xl lg:text-7xl z-20">
+                I'm a &nbsp;
+              </div>
+              <div className="text-2xl md:text-5xl lg:text-7xl  z-20 text-yellow-500">
+                Software Engineer
+              </div>
+            </div>
+            <div className=" z-20 mt-12 md:mt-20 w-4/5 md:w-1/2 text-base lg:text-lg">
               I'm a 22 years old french student in the engineering school
               Grenoble INP Ensimag and I study computer science.
             </div>
-            <button className="z-20" onClick={() => scrollToToProject()}>
+            <button className="z-20 mt-20" onClick={() => scrollToToProject()}>
               GET STARTED
             </button>
           </div>
