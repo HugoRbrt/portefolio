866cdd6c90b339582ca6c8fd7dfdb4442f2e8329
hugo ROBERT <hugo@macbook-air.home>
2023-01-27 00:34:15 +0100

    fix pbm of scrollbar

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="overflow-hidden">
+      <App />
+    </div>
   </React.StrictMode>,
 );
